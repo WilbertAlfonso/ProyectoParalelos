990c8647a2f7c1a432b7a9950dbf7153e73746e7
zafiron <zafiron@gmail.com>
2016-11-02 01:37:03 -0500

    agregado contructor con generador de variables para el rsa

--- RSA-Final/RSA-Final/RSA.h
+++ RSA-Final/RSA-Final/RSA.h
@@ -1,8 +1,11 @@
 #pragma once
-#include <string>
+#include<string>
+#include<iostream>
 #include <NTL/ZZ.h>
-#include <vector>
-#include "aritmetica.h"
+#include<vector>
+#include<algorithm>
+#include <sstream>      // std::stringstream
+#include<fstream>
 using namespace NTL;
 using namespace std;
 class RSA
@@ -16,7 +19,30 @@ public:
 	ZZ e;
 	ZZ d;
 	aritmetica Funciones; //Aqui pondremos todas las funciones que hagamos en paralelo y algunos algortimos.
-	RSA();
+	
+	/**
+	Author: luis
+	Constructor rsa
+	int bits: es el numero de bits que tendra cada numero generado
+	datos: es el abecedario de caracteres 
+	*/
+	RSA(int bits):datos(" abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    {
+
+        do
+        {
+            p=GenPrime_ZZ(bits);
+            q=GenPrime_ZZ(bits);
+        }
+        while(p==q);
+
+        n=p*q;
+        on=(p-1)*(q-1);
+
+            e=GenPrime_ZZ(bits);
+
+        d=InvMod(e,on);
+    }
 	~RSA();
 	ZZ getLlavePublica(){
 		return e;
